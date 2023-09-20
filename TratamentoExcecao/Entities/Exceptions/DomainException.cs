2080b066a780d7378489cebece976dfa643afc7d
Higor Metodio <higor.metodio@outlook.com>
2023-09-20 14:50:14 -0300

    Exercicio Proposto sobre Tratamento de Exceção em CSharp

---
+++ TratamentoExcecao/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+namespace TratamentoExcecao.Entities.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
